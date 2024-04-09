3b2b6392377a96a7a71ee7e80928190bdf3877a2
beuzeville <beuzevillejp@gmail.com>
2024-04-09 15:34:37 -0500

    Add initial project setup
    This commit includes the initial setup of the project. The main components added include Java files containing a simple Spring Boot application setup, Maven wrapper files allowing the use of Maven without having it pre-installed, and necessary .gitignore and property files.

---
+++ src/main/java/com/example/testjenkins2/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.testjenkins2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/testjenkins2/TestJenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkins2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
